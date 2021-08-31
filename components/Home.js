5dab1f8fd39abf64428834bdcaa548f8c5c2d96c
Tihor Nezic <tihor.nezic@gmail.com>
2021-08-31 18:54:56 +0200

    Home styling adjusted

--- components/Home.js
+++ components/Home.js
@@ -12,67 +12,59 @@ const Home = () => {
         <div className='container'>
             <div id='home' className={homeStyles.home}>
 
-                <div className={homeStyles.homeRow}>
-                    <div className={homeStyles.left}>
-                        <h1>Hi, I am Tihor.</h1>
-                        <h1>I am a <span>Frontend</span> Web Dev.</h1>
-                        <h2>"Don't act, <span>React</span>!"</h2>
+                <div className={homeStyles.headings}>
+                    <h1>Hi, I am Tihor.</h1>
+                    <h1>I am a <span>Frontend</span> Web Dev.</h1>
+                    <h2>"Don't act, <span>React</span>!"</h2>
 
-                        <div className={homeStyles.buttonsRow}>
-                            <Link href='#'>
-                                <p className='button buttonPrimary'>View Projects</p>
-                            </Link>
+                    <div className={homeStyles.buttonsRow}>
+                        <Link href='#'>
+                            <p className='button buttonPrimary'>View Projects</p>
+                        </Link>
 
-                            <div className={homeStyles.dash}></div>
+                        <div className={homeStyles.dash}></div>
 
-                            <div className={homeStyles.socialsButtonsRow}>
-                                <div>
-                                    <Link href='#'>
-                                        <p className={homeStyles.socialButton}>
-                                            <Image src={linkedin} alt='linkedin' />
-                                        </p>
-                                    </Link>
-                                </div>
+                        <div className={homeStyles.socialsButtonsRow}>
+                            <div>
+                                <Link href='#'>
+                                    <p className={homeStyles.socialButton}>
+                                        <Image src={linkedin} alt='linkedin' />
+                                    </p>
+                                </Link>
+                            </div>
 
-                                <div>
-                                    <Link href='#'>
-                                        <p className={homeStyles.socialButton}>
-                                            <Image src={github} alt='github' width='21px' />
-                                        </p>
-                                    </Link>
-                                </div>
+                            <div>
+                                <Link href='#'>
+                                    <p className={homeStyles.socialButton}>
+                                        <Image src={github} alt='github' width='21px' />
+                                    </p>
+                                </Link>
+                            </div>
 
-                                <div>
-                                    <Link href='#'>
-                                        <p className={homeStyles.socialButton}>
-                                            <Image src={mail} alt='mail' width='23px' />
-                                        </p>
-                                    </Link>
-                                </div>
+                            <div>
+                                <Link href='#'>
+                                    <p className={homeStyles.socialButton}>
+                                        <Image src={mail} alt='mail' width='23px' />
+                                    </p>
+                                </Link>
+                            </div>
 
-                                <div>
-                                    <Link href='#'>
-                                        <p className={homeStyles.socialButton}>
-                                            <Image src={cv} alt='cv' width='23px' />
-                                        </p>
-                                    </Link>
-                                </div>
+                            <div>
+                                <Link href='#'>
+                                    <p className={homeStyles.socialButton}>
+                                        <Image src={cv} alt='cv' width='23px' />
+                                    </p>
+                                </Link>
                             </div>
                         </div>
                     </div>
-
-                    <div className={homeStyles.right}>
-                        {<div className={homeStyles.imageWrapper}>
-                            <Image src={rocket} alt='Rocket' className={homeStyles.image} />
-                        </div>}
-                    </div>
-
                 </div>
+
+                <div className={homeStyles.imageWrapper}>
+                    <Image src={rocket} alt='Rocket' className={homeStyles.image} />
+                </div>
+
             </div>
-
-
-
-
         </div>
     )
 }
