caca2899a9976cb2832d17538a9b6895f505ed85
Tihor Nezic <tihor.nezic@gmail.com>
2021-09-01 19:09:45 +0200

    contact api edited

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,34 +1,37 @@
-export default function contact (req, res) {
+export default function contact(req, res) {
     require('dotenv').config()
     let nodemailer = require('nodemailer')
 
-    const transporter = nodemailer.createTransport({
-        port: 465,
-        host: "smtp.gmail.com",
-        auth: {
-            user: 'tihorfrontenddev@gmail.com',
-            // pass: process.env.password,
-            pass: 'reactfrontend',
-        },
-        secure: true,
-    })
+    if (req.method === 'POST') {
+        const transporter = nodemailer.createTransport({
+            port: 465,
+            host: "smtp.gmail.com",
+            auth: {
+                user: 'tihorfrontenddev@gmail.com',
+                // pass: process.env.password,
+                pass: 'reactfrontend'
+            },
+            secure: true,
+        })
 
-    const mailData = {
-        from: 'tihorfrontenddev@gmail.com',
-        to: 'tihor.nezic@gmail.com',
-        subject: `Message From ${req.body.name}`,
-        text: req.body.message + " | Sent from: " + req.body.email,
-        html: `<div>${req.body.message}</div><p>Sent from: ${req.body.email}</p>`
+
+        const mailData = {
+            from: 'tihorfrontenddev@gmail.com',
+            to: 'tihor.nezic@gmail.com',
+            subject: `Message From ${req.body.name}`,
+            text: req.body.message + " | Sent from: " + req.body.email,
+            html: `<div>${req.body.message}</div><p>Sent from: ${req.body.email}</p>`
+        }
+
+        transporter.sendMail(mailData, function (err, info) {
+            if (err)
+                console.log(err)
+            else
+                console.log(info);
+        })
+
+        // console.log(req.body)
+
+        res.send('success')
     }
-
-    transporter.sendMail(mailData, function (err, info) {
-        if (err)
-            console.log(err)
-        else
-            console.log(info);
-    })
-
-    // console.log(req.body)
-
-    res.send('success')
 }
