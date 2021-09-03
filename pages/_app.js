f96e7f6ce9e1942184e2a7f34ef3cbe7fb46b848
Tihor Nezic <tihor.nezic@gmail.com>
2021-09-03 17:17:46 +0200

    Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+/* function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
 export default MyApp
+ */
+
+import {useEffect} from 'react'
+import {useRouter} from 'next/router'
+import * as gtag from '../lib/gtag'
+
+const App = ({Component, pageProps}) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return <Component {...pageProps} />
+}
+
+export default App
