051f72ac04a2775933431603b2f895475dd00d05
Tihor Nezic <tihor.nezic@gmail.com>
2021-09-03 18:15:45 +0200

    Refactored GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,29 @@
 import '../styles/globals.css'
+import Script from 'next/script'
+import React from 'react'
 
-/* function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+  return (
+    <>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+
+      <Script
+        strategy='lazyOnload'>
+        {
+          `window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+    
+           gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');`
+        }
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
- */
-
-import {useEffect} from 'react'
-import {useRouter} from 'next/router'
-import * as gtag from '../lib/gtag'
-
-const App = ({Component, pageProps}) => {
-  const router = useRouter()
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
-
-  return <Component {...pageProps} />
-}
-
-export default App
